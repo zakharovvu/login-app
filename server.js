8096dfce17fd40a332621d07d1551276ab7a90bd
Zakharov Viacheslav <zakharovvu@gmail.com>
2019-04-11 12:41:19 +0300

    init

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', 'http://127.0.0.1:3000')
+  res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
   next();
 });
 
